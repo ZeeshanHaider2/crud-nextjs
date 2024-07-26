b00f4c3ae49e5fd437c42365bace2003b0c3dbbb
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-07-26 20:55:05 +0200

    post api route completed

--- src/components/list-box/index.js
+++ src/components/list-box/index.js
@@ -1,22 +1,52 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const ListBox = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  async function handleSubmit() {
+    // fetch API POST
+    try {
+      const apiResponse = await fetch("/api/list", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      });
+    } catch (error) {}
+    // body data = formData
+  }
   return (
     <div>
-      <form className="flex flex-col">
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
         <label htmlFor="title">Title</label>
         <input
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+          value={formData.title}
           type="text"
           name="title"
           className="text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
         />
         <label htmlFor="description">Description</label>
         <textarea
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          value={formData.description}
           type="text"
           name="description"
           className="h-24 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
         />
-        <button className="px-6 my-4 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-900 border-gray-600 placeholder-gray-400 text-white">
+        <button
+          type="submit"
+          className="px-6 my-4 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-900 border-gray-600 placeholder-gray-400 text-white"
+        >
           Create
         </button>
       </form>
